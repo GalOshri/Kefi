bdd8d65ae6ffafc82542b1bec9faa5b8f966d9fc
p-stav <paulstavropoulos@gmail.com>
2014-05-16 00:29:30 -0700

    adding hashtag collections cell
    hi

---
+++ iOS/Kefi/HashtagCollectionCell.h
@@ -0,0 +1,15 @@
+//
+//  HashtagCollectionCell.h
+//  Kefi
+//
+//  Created by Paul Stavropoulos on 5/15/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HashtagCollectionCell : UICollectionViewCell
+
+@property (strong, nonatomic) IBOutlet UIButton *hashtagToggle;
+
+@end
---
+++ iOS/Kefi/HashtagCollectionCell.m
@@ -0,0 +1,31 @@
+//
+//  HashtagCollectionCell.m
+//  Kefi
+//
+//  Created by Paul Stavropoulos on 5/15/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import "HashtagCollectionCell.h"
+
+@implementation HashtagCollectionCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
